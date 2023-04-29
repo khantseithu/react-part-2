e7ef2cd97b2fdd1febc59bd66fc6bcee40bf47a6
Khant Sithu <khantseithuu@gmail.com>
2023-04-29 08:23:19 +0000

    Posts Challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <TodoList />;
+  return (
+    <>
+      <h1>Todos</h1>
+      <TodoList />
+      <h1>Posts</h1>
+      <PostList />
+    </>
+  );
 }
 
 export default App;
